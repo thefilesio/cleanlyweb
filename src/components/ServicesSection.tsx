6817c3418f39832f496d0db7065b6708f5c9ec22
TheFiles <info@thefiles.io>
2025-09-14 19:56:21 +0200

    Update ServicesSection.tsx

--- src/components/ServicesSection.tsx
+++ src/components/ServicesSection.tsx
@@ -54,7 +54,7 @@ const ServicesSection = () => {
 
   return (
     <section className="py-20 bg-clean-white">
-      <div className="container mx-auto px-4">
+      <div className="container mx-auto px-6 sm:px-4">
         <div className="max-w-6xl mx-auto">
           {/* Header */}
           <div className="text-center mb-16">
